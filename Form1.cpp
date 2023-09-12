bd8ed5e272974677aca80f4fb8f36dfe8dbafe85
さくらみこ <lin@sz.cn.eu.org>
2023-09-12 15:02:27 +0300

    Update Form1.cpp

--- Form1.cpp
+++ Form1.cpp
@@ -3,7 +3,9 @@
 #include <QDir>
 #include <QSize>
 #include <QWebEngineView>
-
+#include <QFile>
+#include <QString>
+#include <QTextStream>
 class Form1 : public QMainWindow
 {
 public:
@@ -69,3 +71,22 @@ private slots:
         }
     }
 };
+qint64 GetLastUpdateServerTime()
+{
+    QString filePath = "data/last_update.v";
+    QFile file(filePath);
+    if (file.exists())
+    {
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QTextStream in(&file);
+            QString text = in.readAll();
+            file.close();
+            bool ok;
+            qint64 lastUpdateServerTime = text.toLongLong(&ok);
+            if (ok)
+                return lastUpdateServerTime;
+        }
+    }
+    return 0;
+}
